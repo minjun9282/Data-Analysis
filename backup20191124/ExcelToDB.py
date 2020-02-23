c72f72997a5e9b8c6ab8e4a69ad0d915983f9628
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2020-02-23 16:54:42 +0900

    Update ExcelToDB.py

--- backup20191124/ExcelToDB.py
+++ backup20191124/ExcelToDB.py
@@ -6,4 +6,5 @@ def excel_to_db(filename, db_name, tablename):
     df = pd.read_excel('%s' %filename)
     df.to_sql('%s' %tablename, conn)
 
-excel_to_db('real_deal202001.xls', 'new_real_deal202021.db', 'deal')
+excel_to_db('real_deal202001.xls', 'new_real_deal202001.db', 'deal')
+# 잠시 실거래가 db를 최신 자료로 업데이트 해주기 위해 사용한 간단한 코드임. 아파트 매매가격의 숫자 구분자 ','로 인한 오류는 일단 엑셀의 기능으로 수정함.
